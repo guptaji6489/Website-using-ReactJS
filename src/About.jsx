a03bad90c8a51e59045940372c1969ba3fe1c1ba
guptaji6489 <guptaji6489@gmail.com>
2020-07-05 00:30:20 +0530

    complete about page  with all functionality and make a common page for home page and about page and from common page we are passing props and changeing our contents

--- src/About.jsx
+++ src/About.jsx
@@ -1,38 +1,14 @@
 import React from 'react';
-import home from '../src/images/home';
-import { NavLink } from 'react-router-dom';
+import Common from './Common';
+import web1 from '../src/images/home';
+
 
 
 
 const About = () =>{
   return(
     <>
-    <section id="header" className="d-flex align-items-center">
-    <div className="container-fluid nav-bg">
-        <div className="row">
-            <div className="col-10 mx-auto">
-            <div className="row">
-            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-            <h1>Grow your business with<strong className="brand-name"> Guptaji Solutions</strong> </h1>
-            <h2 className="my-3">
-                We are a team of talented web developers.
-            </h2>
-            <div className="mt-3">
-            <NavLink to='/service' className="btn-get-started"> Get Started</NavLink>
-            </div>
-            </div>
-
-            <div className="col-lg-6 order-1 order-lg-2 header-img">
-                <img src={home} className="img-fluid animated" alt="home_img" />
-            </div>
-            </div>
-
-            </div>
-        </div>
-    </div>
-
-    </section>
-
+    <Common name="Welcome to About Page" imgsrc={web1} visit='/contact' btname="Contact Now" />
     </>
 
   );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () =>{
             <div className="col-10 mx-auto">
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-  <NavLink  className="navbar-brand" to="/">Guptaji</NavLink>
+  <NavLink  className="navbar-brand" to="/">Guptaji Solutions</NavLink>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -22,11 +22,12 @@ const Navbar = () =>{
         <NavLink activeClassName="menu_active" className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
       </li>
       <li className="nav-item">
-        <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
-      </li>
-      <li className="nav-item">
         <NavLink activeClassName="menu_active" className="nav-link" to="/service">Service</NavLink>
       </li>
+      <li className="nav-item">
+        <NavLink activeClassName="menu_active" className="nav-link" to="/about">About</NavLink>
+      </li>
+      
       <li className="nav-item">
         <NavLink activeClassName="menu_active" className="nav-link" to="/contact">Contact</NavLink>
       </li>
